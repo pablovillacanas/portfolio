58cf3ed928e52ea135870d82af110ce2dd1ea68d
Pablo.Villacanas <pablo.villacanas@gmail.com>
2022-11-05 23:18:32 +0100

    Use mouse cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { Environment } from "@react-three/drei";
 import { Suspense } from "react";
 
 import "./styles.css";
-import { Model } from "./background/Model";
+import Portfoliobackground from "./background/Portfoliobackground";
+import useMove from "./hooks";
 
 export default function App() {
+  const { x, y, handleMouseMove } = useMove();
+
   return (
-    <div className="App">
-      <Canvas>
+    <div className="App" onMouseMove={handleMouseMove}>
+      <Canvas shadows>
         <Suspense fallback={null}>
-          <Model />
-          <Environment preset="sunset" background />
+          <spotLight position={[50, 50, 10]} angle={0.15} penumbra={1} />
+          <Portfoliobackground x={x} y={y} />
         </Suspense>
       </Canvas>
     </div>
