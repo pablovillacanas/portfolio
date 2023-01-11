deda8a6c207ff3639b47be20c8eba4ce64719774
Pablo.Villacanas <pablo.villacanas@gmail.com>
2023-01-11 12:34:15 +0100

    change metadata

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import React from "react";
 import App from "../components/App";
 import styles from "../styles/Home.module.css";
@@ -6,6 +7,10 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className="main" style={{ height: "100%" }}>
+        <Head>
+          <title>Pablo Villacañas</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <App></App>
       </div>
     </div>
