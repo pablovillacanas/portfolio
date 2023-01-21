032463d149ff6596f3167366f6b78f1677eec682
Pablo.Villacanas <pablo.villacanas@gmail.com>
2023-01-21 19:05:16 +0100

    Undo FF

--- components/App.tsx
+++ components/App.tsx
@@ -66,8 +66,8 @@ export default function App() {
         };
       default:
         return {
-          title: true ? "" : "Hi, I'm Pablo!",
-          subtitle: true ? "" : "Get to know me a little better :D",
+          title: "Hi, I'm Pablo!",
+          subtitle: "Get to know me a little better :D",
           content: <HomeContent />,
         };
     }
