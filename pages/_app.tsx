edbb4f77669e2bea1a572be5214bcd56e7344c3c
Pablo.Villacanas <pablo.villacanas@gmail.com>
2023-01-25 10:16:18 +0100

    Set analytics vercel package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import "../styles/globals.css";
 import "../styles/fonts.css";
 import type { AppProps } from "next/app";
 import React from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
