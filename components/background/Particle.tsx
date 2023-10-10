327fff2cfb6c23bc2cca35b2c37e286062f6046e
Pablo.Villacanas <pablo.villacanas@gmail.com>
2023-10-10 15:47:10 +0200

    feat: starfield

---
+++ components/background/Particle.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef } from "react";
+import * as THREE from "three";
+
+export interface ParticleProps {
+  initialPosition: [number, number, number];
+  endPosition: [number, number, number];
+  vector: "horizontal" | "vertical";
+}
+
+const Particle = (props: ParticleProps) => {
+  const particleRef = useRef<THREE.Mesh>(null);
+
+  useEffect(() => {
+    // Animation loop
+    const animate = () => {
+      requestAnimationFrame(animate);
+      // Go from initial position to end position in 200ms and then delete the particle
+      if (particleRef.current) {
+        particleRef.current.position.lerp(
+          new THREE.Vector3(...props.endPosition),
+          6 * 0.001
+        );
+        if (
+          particleRef.current.position.distanceTo(
+            new THREE.Vector3(...props.endPosition)
+          ) == 0
+        ) {
+          particleRef.current.parent?.remove(particleRef.current);
+        }
+      }
+    };
+    if (particleRef.current) {
+      animate();
+    }
+  }, []);
+
+  return (
+    <mesh
+      position={props.initialPosition}
+      scale={[0.02, 0.02, 0.02]}
+      ref={particleRef}
+    >
+      <sphereGeometry />
+      <pointLight
+        intensity={1}
+        distance={0.7}
+        power={4}
+        shadow-mapSize-height={512}
+        shadow-mapSize-width={512}
+      />
+      <meshStandardMaterial
+        metalness={0}
+        color={"#bea925"}
+        emissive={"#bea925"}
+        emissiveIntensity={0.5}
+      />
+    </mesh>
+  );
+};
+
+export default Particle;
---
+++ components/background/particlesFactory.ts
@@ -0,0 +1,48 @@
+import * as THREE from "three";
+import { ParticleProps } from "./Particle";
+
+//z desde 3 a -10
+//x de -10 a -1
+
+const HEIGHT = 0.03;
+
+export const verticalVector = new THREE.Vector3(-6, HEIGHT, 0.49)
+  .clone()
+  .sub(new THREE.Vector3(0.04, 0, 0.915));
+
+export const horizontalVector = new THREE.Vector3(0.17, HEIGHT, -0.855).sub(
+  new THREE.Vector3(0.04, 0, 0.915)
+);
+
+export const createParticles = (num: number): ParticleProps[] => {
+  const particles = [];
+  for (let i = 0; i < num; i++) {
+    const init = new THREE.Vector3(0, HEIGHT, 0)
+      .clone()
+      .add(new THREE.Vector3(0, 0, Math.random() * (3 - -10) + -10))
+      .clone()
+      .add(new THREE.Vector3(Math.random() * (-10 - -1) + -1));
+    const direction = Math.random() > 0.5 ? "vertical" : "horizontal";
+    const sense = Math.random() > 0.5 ? 1 : -1;
+    let endPosition;
+    if (direction === "vertical") {
+      endPosition = init
+        .clone()
+        .add(verticalVector.clone().multiplyScalar(Math.random() * 2 * sense));
+    } else {
+      endPosition = init
+        .clone()
+        .add(
+          horizontalVector.clone().multiplyScalar(Math.random() * 2 * sense)
+        );
+    }
+    endPosition.y = HEIGHT;
+    particles.push({
+      initialPosition: init.toArray() as [number, number, number],
+      endPosition: endPosition,
+      vector: "vertical" as const,
+    });
+  }
+  console.log("particles :>> ", particles);
+  return particles;
+};
