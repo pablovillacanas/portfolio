f85e4444beab30d20d1c83c2fc4c100dbc9151ed
Pablo.Villacanas <pablo.villacanas@gmail.com>
2023-12-18 18:04:50 +0100

    feat: add verification and some thing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,36 @@
-import Head from 'next/head';
-import React from 'react';
-import App from '../components/App';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import React from "react";
+import App from "../components/App";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
-	return (
-		<div className={styles.container}>
-			<div className='main' style={{ height: '100%' }}>
-				<Head>
-					<title>Pablo Villacañas</title>
-					<link rel='icon' href='/favicon.ico' />
-					<meta
-						name='title'
-						content='Pablo Domínguez Villacañas freelance full-stack developer'
-					></meta>
-					<meta
-						name='facebook-domain-verification'
-						content='judoa61fzepajdwo2i9e0ep12mksus'
-					/>
-					<meta
-						name='description'
-						content='Pablo Dominguez Villacañas is a freelance full-stack developer based in Spain'
-					></meta>
-				</Head>
-				<App></App>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <div className="main" style={{ height: "100%" }}>
+        <Head>
+          <title>Pablo Villacañas</title>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="title"
+            content="Pablo Domínguez Villacañas freelance full-stack developer"
+          ></meta>
+          <meta
+            name="facebook-domain-verification"
+            content="judoa61fzepajdwo2i9e0ep12mksus"
+          />
+          <meta
+            name="description"
+            content="Pablo Dominguez Villacañas is a freelance full-stack developer based in Spain"
+          ></meta>
+        </Head>
+        <App></App>
+      </div>
+    </div>
+  );
 }
 
 export async function getServerSideProps() {
-	return {
-		props: {},
-	};
+  return {
+    props: {},
+  };
 }
